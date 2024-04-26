a2a2a0fc36562fd9195b6f4eef6260ad0445965f
Nicolas <cazenicolas64@gmail.com>
2024-04-26 14:35:05 +0200

    modif ortho et toggle button

--- components/toggle-theme.tsx
+++ components/toggle-theme.tsx
@@ -4,20 +4,23 @@ import { Button } from "./ui/button";
 import { Moon, Sun } from "lucide-react";
 
 export default function ToggleTheme() {
-  const {theme, setTheme} = useTheme();
-    const Toggle  = () => {
-        setTheme(theme === "dark" ? "light" : "dark");
-    }
-    return (
-        <Button 
-        className="flex justify-center"
-        variant="ghost"
-        size="icon"
-        onClick={Toggle}
-        >
-            <Moon className="h-6 w-6 scale-100 dark:scale-0" />
-            <Sun className="h-6 w-6 scale-0 dark:scale-100" />
-            
-        </Button>
-    )
+  const { theme, setTheme } = useTheme();
+
+  const Toggle = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <Button
+      className="flex justify-center items-center"
+      variant="ghost"
+      size="icon"
+      onClick={Toggle}
+    >
+      <div className="flex justify-center items-center w-6 h-6">
+        <Moon className="dark:hidden" />
+        <Sun className="hidden dark:block" />
+      </div>
+    </Button>
+  );
 }
